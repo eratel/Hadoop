c560f9e455896e5c823a1ffa98aaf747875bec4e
jikai.sun <sunjk@cn95598.com>
2018-08-03 11:36:59 +0800

    HDFS测试

--- hdfs/src/main/java/com/abhsy/hdfs/Hdfs.java
+++ hdfs/src/main/java/com/abhsy/hdfs/Hdfs.java
@@ -1,13 +1,59 @@
 package com.abhsy.hdfs;
 
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.LocatedFileStatus;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.RemoteIterator;
+
+import java.io.IOException;
+
 /**
  * @program: hadoop
  * @author: jikai.sun
  * @create: 2018-07-30
  **/
 public class Hdfs {
-    public static void main(){
 
+    public static void main(String arg[]) throws IOException {
+//        如果没有配置环境变量可以添加Property
+//        System.setProperty("hadoop.home.dir", "D:/hadoop-2.6.1");
+        Configuration conf = new Configuration();
+        conf.set("fs.defaultFS","hdfs://hadoop1:9000");
+//        FileSystem是抽象的  无法New(很多厂家的实现So不能NEW会有其他厂家的实现)
+        FileSystem fs = FileSystem.get(conf);
+        //recursive 是否递归
+        RemoteIterator<LocatedFileStatus> listFiles = fs.listFiles(new Path("/"), false);
+        //通过迭代器拿到hdfs文件系统的文件
+        while (listFiles.hasNext()){
+            LocatedFileStatus fileStatus = listFiles.next();
+            Path path = fileStatus.getPath();
+            String name = path.getName();
+            System.out.printf("---------------" + name);
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
