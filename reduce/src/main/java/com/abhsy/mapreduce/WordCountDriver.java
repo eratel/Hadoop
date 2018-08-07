e971d1b9d985ddac13fb634776f2cd745aab97e9
jikai.sun <sunjk@cn95598.com>
2018-08-07 11:25:02 +0800

    添加mapReduce

--- reduce/src/main/java/com/abhsy/mapreduce/WordCountDriver.java
+++ reduce/src/main/java/com/abhsy/mapreduce/WordCountDriver.java
@@ -1,5 +1,17 @@
 package com.abhsy.mapreduce;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+import javax.xml.soap.Text;
+import java.io.IOException;
+
 /**
  * @program: abhsy-hadoop
  * @author: jikai.sun
@@ -7,4 +19,76 @@ package com.abhsy.mapreduce;
  **/
 public class WordCountDriver {
 
+    public static void main(String [] args) throws IOException, ClassNotFoundException, InterruptedException {
+        Configuration configuration = new Configuration();
+        configuration.set("fs.defaultFS", "hdfs://hadoop1:9000");
+        Job job = Job.getInstance(configuration);
+        //告诉框架，我们的的程序所在jar包的位置
+//        job.setJar("/root/reduce.jar");
+        job.setJarByClass(WordCountDriver.class);
+        job.setMapperClass(WordCountMapper.class);
+        job.setReducerClass(WordCountReduce.class);
+        //添加MAP输入值的类型
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(IntWritable.class);
+        //设置整个输出类型
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        //设置数据读取组件,结果输出组件
+        job.setInputFormatClass(TextInputFormat.class);
+        job.setOutputFormatClass(TextOutputFormat.class);
+        //设置处理文件路径  FileInputFormat要使用reduce包下面的
+        FileInputFormat.setInputPaths(job,new Path("/hadoop/capacity-scheduler.xml"));
+        FileOutputFormat.setOutputPath(job,new Path("/1.txt"));
+        /**
+         * 等待客户端返回是否完成
+         */
+        boolean res = job.waitForCompletion(true);
+        /**
+         *  如果返回true就退出
+         */
+        System.exit(res?0:1);
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
