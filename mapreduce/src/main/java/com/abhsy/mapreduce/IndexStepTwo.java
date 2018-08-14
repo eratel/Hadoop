b6db50c72462776edbbc94f5d1e5441a2ae6d748
jikai.sun <sunjk@cn95598.com>
2018-08-14 11:40:12 +0800

    hadoop 更新

--- mapreduce/src/main/java/com/abhsy/mapreduce/IndexStepTwo.java
+++ mapreduce/src/main/java/com/abhsy/mapreduce/IndexStepTwo.java
@@ -2,7 +2,6 @@ package com.abhsy.mapreduce;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
