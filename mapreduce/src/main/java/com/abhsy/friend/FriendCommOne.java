ed3cdc7ef4e484ee51882c06750a2e6a6148cb0c
jikai.sun <sunjk@cn95598.com>
2018-08-14 13:55:11 +0800

    hadoop 更新

--- mapreduce/src/main/java/com/abhsy/friend/FriendCommOne.java
+++ mapreduce/src/main/java/com/abhsy/friend/FriendCommOne.java
@@ -1,15 +1,13 @@
-package com.abhsy.mapreduce2;
+package com.abhsy.friend;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
-import org.apache.hadoop.mapreduce.lib.input.FileSplit;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
--- mapreduce/src/main/java/com/abhsy/friend/FriendCommTwo.java
+++ mapreduce/src/main/java/com/abhsy/friend/FriendCommTwo.java
@@ -1,8 +1,7 @@
-package com.abhsy.mapreduce2;
+package com.abhsy.friend;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
